fb0808745029b24d0c2e5d37f2006eaca1f5607d
kyeoul <54390304+kyeoul@users.noreply.github.com>
2025-03-18 18:35:00 -0500

    separating into ring and tower directory

--- software/apps/barad_dur/main.c
+++ software/apps/barad_dur/main.c
@@ -13,29 +13,39 @@
 #include "nrfx_gpiote.h"
 
 #include "microbit_v2.h"
-#include "distance_sensor.h"
 #include "pwm.h"
 #include "servo.h"
 #include "ir_array.h"
-#include "i2c.h"
-#include "temp_sensor.h"
-#include "capacitive_touch.h"
-#include "ir_led.h"
 
 #define IR_LED_PIN EDGE_P0
 #define VOLTAGE_MEASURE_CHANNEL NRF_SAADC_INPUT_AIN4
 APP_TIMER_DEF(distance_timer);
-APP_TIMER_DEF(sample_timer);
+// APP_TIMER_DEF(sample_timer);
 
 // Global variables
 NRF_TWI_MNGR_DEF(twi_mngr_instance, 1, 0);
 
+bool interrupt_table[64] = {0};
 static void saadc_event_handler(nrfx_saadc_evt_t const *p_evt);
-
-static void saadc_event_handler(nrfx_saadc_evt_t const *_unused)
+void ir_led_init(void)
 {
-  // don't care about saadc events
-  // ignore this function
+  nrfx_gpiote_out_config_t out_config = NRFX_GPIOTE_CONFIG_OUT_SIMPLE(false);
+  nrfx_gpiote_out_init(IR_LED_PIN, &out_config);
+}
+
+void ir_led_on(void)
+{
+  nrfx_gpiote_out_set(IR_LED_PIN);
+}
+
+void ir_led_off(void)
+{
+  nrfx_gpiote_out_clear(IR_LED_PIN);
+}
+
+void ir_led_toggle(void)
+{
+  nrfx_gpiote_out_toggle(IR_LED_PIN);
 }
 
 static void adc_init(void)
@@ -90,6 +100,26 @@ static void sample_timer_callback(void *__unused)
 {
   printf("sampling\n");
   printf("temp: %f\n", read_temp_C());
+  // print_temp_array();
+  // if(interrupt_flag_set()){
+  //   printf("interrupt flag set\n");
+  //   for(int i = 0; i < 64; i++){
+  //     interrupt_table[i] = pixel_interrupt_set(i);
+  //   }
+  //   for(int i = 0; i < 8; i++){
+  //     for(int j = 0; j < 8; j++){
+  //       printf("%d ", interrupt_table[i*8 + j]);
+  //     }
+  //     printf("\n");
+  //   }
+  //   clear_interrupt_flag();
+  // }
+}
+
+static void saadc_event_handler(nrfx_saadc_evt_t const *_unused)
+{
+  // don't care about saadc events
+  // ignore this function
 }
 
 int main(void)
@@ -97,28 +127,35 @@ int main(void)
 
   // distance_sensor_init();
   // Initialize I2C peripheral and driver
-  // nrf_drv_twi_config_t i2c_config = NRF_DRV_TWI_DEFAULT_CONFIG;
-  // i2c_config.scl = EDGE_P19;
-  // i2c_config.sda = EDGE_P20;
-  // i2c_config.frequency = NRF_TWIM_FREQ_100K;
-  // i2c_config.interrupt_priority = 0;
-  // nrf_twi_mngr_init(&twi_mngr_instance, &i2c_config);
+  nrf_drv_twi_config_t i2c_config = NRF_DRV_TWI_DEFAULT_CONFIG;
+  i2c_config.scl = EDGE_P19;
+  i2c_config.sda = EDGE_P20;
+  i2c_config.frequency = NRF_TWIM_FREQ_100K;
+  i2c_config.interrupt_priority = 0;
+  nrf_twi_mngr_init(&twi_mngr_instance, &i2c_config);
 
   // distance_sensor_init();
 
   // // initialize i2c client
-  // i2c_init(&twi_mngr_instance);
+  i2c_init(&twi_mngr_instance);
+
+  // set_interrupt_mode_absolute();
+  // set_upper_interrupt_value(50);
+  // set_lower_interrupt_value(0);
+  // set_hysteresis_interrupt_value(5);
 
   app_timer_init();
   pwm_init();
 
-  // servo_init();
+  servo_init();
 
-  // servo_rotate();
+  // servo_write(50);
+
+  speaker_play(CANNOT_HIDE);
 
   // // initialize app timers
   nrfx_gpiote_init();
-  app_timer_init();
+  // app_timer_init();
   // app_timer_create(&sample_timer, APP_TIMER_MODE_REPEATED, sample_timer_callback);
   // app_timer_create(&distance_timer, APP_TIMER_MODE_REPEATED, distance_timer_callback);
 
@@ -133,25 +170,29 @@ int main(void)
   adc_init();
   while (1)
   {
+    // servo_rotate();
+    nrf_delay_ms(100);
+
+    print_temp_array();
     // Turn IR LED ON
-    ir_led_on();
-    nrf_delay_ms(500);
+    // ir_led_on();
+    // nrf_delay_ms(500);
 
-    // Read voltage at P1
-    float voltage_on = read_voltage();
-    printf("IR LED ON - Measured Voltage: %.2f V\n", voltage_on);
+    // // Read voltage at P1
+    // float voltage_on = read_voltage();
+    // printf("IR LED ON - Measured Voltage: %.2f V\n", voltage_on);
 
-    // Turn IR LED OFF
-    ir_led_off();
-    nrf_delay_ms(500);
+    // // Turn IR LED OFF
+    // ir_led_off();
+    // nrf_delay_ms(500);
 
-    // Read voltage at P1 again
-    float voltage_off = read_voltage();
-    printf("IR LED OFF - Measured Voltage: %.2f V\n", voltage_off);
+    // // Read voltage at P1 again
+    // float voltage_off = read_voltage();
+    // printf("IR LED OFF - Measured Voltage: %.2f V\n", voltage_off);
 
-    // printf("Capacitive Touch Active: %d\n", capacitive_touch_is_active());
+    // // printf("Capacitive Touch Active: %d\n", capacitive_touch_is_active());
 
-    nrf_delay_ms(1000);
-    printf("%d\n", capacitive_touch_is_active());
+    // nrf_delay_ms(1000);
+    // printf("%d\n", capacitive_touch_is_active());
   }
 }
